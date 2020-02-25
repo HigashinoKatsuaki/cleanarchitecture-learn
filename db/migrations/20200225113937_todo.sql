094aa0207b6cbefea71d0473525563b452d2c9ea
Higashino <ukikkiosaru@icloud.com>
2020-02-25 12:34:52 +0900

    シード修正

--- db/migrations/20200225113937_todo.sql
+++ db/migrations/20200225113937_todo.sql
@@ -5,8 +5,11 @@ CREATE TABLE todos (
   id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'プライマリーキー',
   status VARCHAR(16) NOT NULL COMMENT 'ステータス',
   task VARCHAR(32) NOT NULL COMMENT 'タスク',
-  limit_date DATETIME NOT NULL COMMENT '期限'
+  limit_date DATETIME NOT NULL DEFAULT '9999-12-31 23:59:59' COMMENT '期限'
 );
+
+INSERT INTO todos(status, task, limit_date) VALUES('completed', 'hoge', '2018-01-08 08:00:00');
+INSERT INTO todos(status, task) VALUES('in progress', 'foo');
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
