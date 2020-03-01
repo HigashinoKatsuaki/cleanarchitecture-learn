6c352ea97b1858df85b8f152b415a5a2b8dd00c7
Higashino <ukikkiosaru@icloud.com>
2020-03-01 11:10:22 +0900

    todoを取得し表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,29 @@
-import { FC } from 'react'
+import { FC, useState, useEffect, Dispatch, SetStateAction } from 'react'
+import axios from 'axios'
 
-const IndexPage: FC<null> = () => <h1>Hello</h1>
+const requesTodos = async(seter: Dispatch<SetStateAction<{[key: string]: string}[]>>) => {
+  const res = await axios.get<{[key: string]: string}[]>('http://localhost:9001/todos')
+  console.log(res.data)
+
+  seter(res.data)
+}
+
+const IndexPage: FC<null> = () => {
+  const [todos, setTodos] = useState<{[key: string]: string}[]>([])
+
+  useEffect(() => {
+    if (todos.length == 0) {
+      requesTodos(setTodos)
+    }
+  })
+
+  const datas = todos.map(data => {
+    return <p>{data.task}</p>
+  })
+
+  return <div>
+    <p> {datas} </p>
+  </div>
+}
 
 export default IndexPage
